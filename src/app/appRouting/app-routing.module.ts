778462760ec9318356cc5facc06b25cf17a1a5b0
ayedih <ayedih123@gmail.com>
2019-12-13 14:40:22 +0100

    1 file modified

--- src/app/appRouting/app-routing.module.ts
+++ src/app/appRouting/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes : Routes = [
+  { path : '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: []
 })
