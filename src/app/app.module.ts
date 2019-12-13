19ab6c3182fbc2098eec84a951f78f6337199e31
ayedih <ayedih123@gmail.com>
2019-12-13 13:58:47 +0100

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
